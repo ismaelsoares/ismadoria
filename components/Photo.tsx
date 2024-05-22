5720e9dc83effbc5dff3c3d9a95589c407e09873
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-05-21 21:41:07 -0300

    Fix div home

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -13,13 +13,14 @@ export const Photo = () => {
           transition: { delay: 2, duration: 0.4, ease: easeIn },
         }}
       >
+        {/* image */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            transition: { delay: 2.4, duration: 0.4, ease: easeInOut },
+            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="size-[298px] xl:size-[498px] mix-blend-lighten"
+          className="size-[298px] xl:size-[498px] mix-blend-lighten absolute"
         >
           <Image
             src="/assets/avatar.png"
@@ -30,6 +31,34 @@ export const Photo = () => {
             className="object-contain"
           />
         </motion.div>
+
+        {/* circle */}
+        <motion.svg
+          className="w-[300px] h-[300px] xl:w-[506px] xl:h-[506px]"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0 " }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
